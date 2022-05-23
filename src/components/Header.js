1e953c0300d57ebbaa1debf06e00b6920d744c5e
Safira Tyas Wandita <safeeratyas@gmail.com>
2022-05-23 12:10:59 +0700

    add components: header, navbar, footer

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { 
+Container,
+Navbar, 
+Nav,
+Button
+} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Header.css';
+
+class Header extends React.Component {
+    render() {
+      return (
+    <Navbar className='navbar' collapseOnSelect bg="light" variant="light">
+        <Container>
+            <Navbar.Brand href="#home" className='logo'></Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+            <Navbar.Collapse className='menu' id="responsive-navbar-nav">
+                <Nav className="ms-auto">
+                <Nav.Link href="#our-services">Our Services</Nav.Link>
+                <Nav.Link href="#why-us">Why Us</Nav.Link>
+                <Nav.Link href="#testimonial">Testimonial</Nav.Link>
+                <Nav.Link href="#faq">FAQ</Nav.Link>
+                <Button variant="success">Success</Button>
+                </Nav>
+            </Navbar.Collapse>
+        </Container>
+    </Navbar>
+      );
+    }
+  }
+  
+  export default Header;
+  
---
+++ src/components/Navbar.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { 
+Container,
+Row,
+Col,
+Button
+} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Header.css';
+
+class Nav extends React.Component {
+    render() {
+      return (
+        <Container className='navBody'>
+        <Row>
+          <Col xs={6}>
+            <h1>Sewa & Rental Mobil Terbaik di Kawasan Jakarta</h1>
+            <p>Selamat datang di Binar Car Rental. Kami menyediakan mobil kualitas terbaik dengan harga terjangkau.
+               Selalu siap melayani kebutuhanmu untuk sewa mobil selama 24 jam.</p>
+            <Button variant="success">Mulai Sewa Mobil</Button>
+          </Col>
+          <Col xs={6}><img src={process.env.PUBLIC_URL + '/images/img_car.png'} /> </Col>
+        </Row>
+      </Container>
+      );
+    }
+  }
+  
+  export default Nav;
+  
